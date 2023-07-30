5af2e34b2a858c91626ed878dc0ac71374db1b62
Mir-sad <mirsadgashi21@gmail.com>
2023-07-30 14:25:44 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./components/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleCart = (item) => {
+    const ndx = cart.findIndex((e) => e.id === item.id);
+    if (ndx !== 1) {
+      setCart([...cart, item]);
+    }
+    console.log(cart);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header size={cart.length} />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<Home handleCart={handleCart} size={cart.length} />}
+          />
+          <Route path={`/product/:productId`} element={<ProductDetail handleCart={handleCart}/>} />
+          <Route
+            path="/cart"
+            element={
+              <Cart handleCart={handleCart} cart={cart} setCart={setCart}  />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
